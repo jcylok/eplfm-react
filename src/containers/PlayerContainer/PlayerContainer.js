f3c68dc03aa31a1e3a260251a69d3b38878eb7bd
jacklok <jack22hk@gmail.com>
2019-12-07 14:42:48 -0800

    pull data from third party api for player details

--- src/containers/PlayerContainer/PlayerContainer.js
+++ src/containers/PlayerContainer/PlayerContainer.js
@@ -1,48 +1,45 @@
 import React, {Component} from 'react'
 import axios from 'axios';
+import PlayerInfo from '../../components/PlayerInfo/PlayerInfo';
 
 class PlayerContainer extends Component {
   state = {
-    postIds: [],
-    cityName: '',
-    countryName: '',
-    picture: '',
-    cityId: '',
-    posts: [],
-    citiesList: [],
-    cityObjId: '',
+    playerInfoNew: {},
+    playerInfoOld: {},
   }
 
   componentDidMount () {
-    axios.get(`${process.env.REACT_APP_API_URL}/cities/${window.location.pathname.split('/')[2]}`)
-      .then((res) => {
-        this.setState({
-          cityName: res.data.data.name,
-          countryName: res.data.data.country,
-          picture: res.data.data.picture,
-          cityId: window.location.pathname.split('/')[2],
-          postIds: res.data.data.posts,
-          cityObjId: res.data.data._id,
+    axios({
+        "method":"GET",
+        "url":`https://api-football-v1.p.rapidapi.com/v2/players/player/${window.location.pathname.split('/')[2]}/2018-2019`,
+        "headers":{
+        "content-type":"application/octet-stream",
+        "x-rapidapi-host":"api-football-v1.p.rapidapi.com",
+        "x-rapidapi-key":"ae34dc57cbmshfbd27c9d54b4ae0p157852jsnb36c3e4228b5"
+        }
         })
-        this.grabPosts();
-        this.grabCitiesList();
-      })
-      .catch((err) => console.log(err));
+        .then((response)=>{
+          console.log(response)
+          this.setState({
+              playerInfoNew: response.data.api.players[0]
+          })
+          if (response.data.api.players[1]) {
+              this.setState({
+                playerInfoOld: response.data.api.players[1]
+              })    
+          }
+        })
+        .catch((error)=>{
+          console.log(error)
+        })
+    
   }
+
 
   render () {
     return (
       <>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm">   
-            {}
-          </div>
-          <div className="col-sm"> 
-
-          </div>
-        </div>
-      </div>
+        <PlayerInfo infoNew={this.state.playerInfoNew} infoOld={this.state.playerInfoOld}/>
       </>
     );
   };
