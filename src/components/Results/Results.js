a4286c2763c47cb0245f16c9e268831bafcd6b55
jacklok <jack22hk@gmail.com>
2019-12-06 16:04:09 -0800

    worked on search function

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -4,10 +4,12 @@ import './Results.css'
 const Results = props => {
     return (
         <div className="col-md-4 resultPlayer">
+            <div className="playerCard">
             <p>{props.playerdata.firstname} {props.playerdata.lastname}</p>
-                <img mode='fit'
-                    src="https://resources.premierleague.com/photos/2019/06/20/056b6aca-a668-4b77-aeb8-a18516e8e5b6/LIV_HK_516.png?width=260&height=330"
-                    alt="jersey" />
+            <img mode='fit'
+                src="https://resources.premierleague.com/photos/2019/06/20/056b6aca-a668-4b77-aeb8-a18516e8e5b6/LIV_HK_516.png?width=260&height=330"
+                alt="jersey" />
+            </div>
         </div>
 
     )
--- src/containers/MarketContainer/MarketContainer.js
+++ src/containers/MarketContainer/MarketContainer.js
@@ -3,7 +3,7 @@ import Search from "../../components/Search/Search";
 import Results from "../../components/Results/Results";
 import axios from "axios";
 import './MarketContainer.css';
-// import api from './sample.json';
+import teamSeed from '../../seed/team.json';
 
 class MarketContainer extends React.Component {
 
@@ -11,59 +11,75 @@ class MarketContainer extends React.Component {
         query: "",
         submitted: false,
         response: {},
+        teamID: "",
     }
+
+    nameToID = (name) => {
+        let newName = name.replace(/ /g,'').toLowerCase();
+        for (let i=0; i<2; i++){
+            if (newName === teamSeed[i].name) {
+                let test = teamSeed[i].team_id;
+                return test
+            }
+        }
+        return('oops')
+
+    };
 
     handleInput = (event) => {
         event.preventDefault();
         this.setState(
             {
                 query: event.target.value,
-                submitted: false
+                submitted: false,
             }  
         )
-        // this.search();
+        // this.setState({
+        //     teamID: this.nameToID(this.state.query)
+        // })
     }
 
     handleSubmit = (event) => {
         event.preventDefault();
-        this.setState(
-            {
-                submitted: true,
-            }
-        )
-        // console.log(this.state.query)
+        // this.setState(
+        //     {
+        //         submitted: true,
+        //     }
+        // )
+        console.log(this.state.query)
+        // let queryID = this.nameToID(this.state.query)
 
-        // axios.get(`https://api-football-v1.p.rapidapi.com/v2/players/squad/40/2019-2020`)
-        // .then(res => {
-        //     console.log(res.data)
-        //     this.setState({
-        //         response: res.data
-        //     })
-        // })
-        // .catch(err => {
-        //     console.log(err);
-        // })
-
-        axios({
-            "method":"GET",
-            "url":"https://api-football-v1.p.rapidapi.com/v2/players/squad/40/2019-2020",
-            "headers":{
-            "content-type":"application/octet-stream",
-            "x-rapidapi-host":"api-football-v1.p.rapidapi.com",
-            "x-rapidapi-key":"ae34dc57cbmshfbd27c9d54b4ae0p157852jsnb36c3e4228b5"
-            }
-            })
-            .then((res)=>{
-              console.log(res.data.api.players)
-              this.setState({
-                response: res.data.api.players
-              })
-            })
-            .catch((error)=>{
-              console.log(error)
-            })
+        // var results = teams.filter(function (team) { return team.name === "manchesterunited"; });
+        // console.log(results[0].team_id)
+        this.setState({
+            teamID: this.nameToID(this.state.query)
+        })
+        if (typeof this.nameToID(this.state.query) == 'number') {
+            
+            axios({
+                "method":"GET",
+                "url":`https://api-football-v1.p.rapidapi.com/v2/players/squad/${this.nameToID(this.state.query)}/2019-2020`,
+                "headers":{
+                "content-type":"application/octet-stream",
+                "x-rapidapi-host":"api-football-v1.p.rapidapi.com",
+                "x-rapidapi-key":"ae34dc57cbmshfbd27c9d54b4ae0p157852jsnb36c3e4228b5"
+                }
+                })
+                .then((res)=>{
+                  console.log(res.data.api.players)
+                  this.setState({
+                    response: res.data.api.players,
+                    submitted: true,
+                  })
+                })
+                .catch((error)=>{
+                  console.log(error)
+                })
 
 
+        } else {
+            return
+        }
     }
 
     shouldComponentUpdate = (nextProps, nextState) => {
@@ -76,13 +92,15 @@ class MarketContainer extends React.Component {
         return false;
     }
 
-    componentDidUpdate = (prevProps, prevState) => {
-        if (prevState.submitted !== this.state.submitted){
-            this.setState({
-                submitted: true
-            })
-        }
-    }
+    // componentDidUpdate = (prevProps, prevState) => {
+    //     if (prevState.submitted !== this.state.submitted){
+    //         this.setState({
+    //             submitted: true
+    //         })
+    //     }
+    // }
+
+    
 
     displayResults = () => {
         
@@ -97,6 +115,7 @@ class MarketContainer extends React.Component {
     }
 
     render () {
+        console.log(teamSeed)
         return (
             <>
                 <div className="searchContainer">
