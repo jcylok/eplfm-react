f67af01ca1ce4dd6cd27987e5e1f65573c6f83d7
jacklok <jack22hk@gmail.com>
2019-12-10 12:31:18 -0800

    login page css

--- src/components/Auth/LoginContainer.js
+++ src/components/Auth/LoginContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './LoginContainer.css';
 
 class LoginContainer extends Component {
     state = {
@@ -36,23 +37,27 @@ class LoginContainer extends Component {
     render() {
         // console.log(this.props)
         return(
-            <div className="container mt-4">
-            <div className="row">
-              <div className="col-md-4 offset-md-4">
-                <form onSubmit={this.handleSubmit}>
-                  <div className="form-group">
-                    <label htmlFor="UserName">Username</label>
-                    <input onChange={this.handleChange} className="form-control form-control-lg" type="text" id="userName" name="userName" value={this.state.userName} />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input onChange={this.handleChange} className="form-control form-control-lg" type="password" id="password" name="password" value={this.state.password} />
-                  </div>
-                  <button id="login-button" onClick={this.handleSubmit} className="btn btn-primary float-right" type="submit">Login</button>
-                </form>
-              </div>
-            </div>
-          </div>        
+            <section class="loginsection ">
+                    <div className="container ">
+                    <div className="row login">
+                    <div className="col-md-4 offset-md-4">
+                        <form onSubmit={this.handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="UserName">Username</label>
+                            <input onChange={this.handleChange} className="form-control form-control-lg" type="text" id="userName" name="userName" value={this.state.userName} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input onChange={this.handleChange} className="form-control form-control-lg" type="password" id="password" name="password" value={this.state.password} />
+                        </div>
+                     
+                            <button id="login-button" onClick={this.handleSubmit} className="btn btn-primary" type="submit">Login</button>
+                
+                        </form>
+                    </div>
+                    </div>
+                </div>
+            </section>        
         );
     };
 };
