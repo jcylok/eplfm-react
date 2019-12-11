e2b9bf61fcefb233b96ff94a6f6aa282c809ee3e
jacklok <jack22hk@gmail.com>
2019-12-11 09:45:58 -0800

    optimize landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,6 +11,8 @@ const Home = () => {
                 <div className="inlineblock">
                     <h2><span className="super-title">E</span>nglish <span className="super-title">P</span>remier <span className="super-title">L</span>eague</h2>
                     <p className="super-title">Football Manager</p>
+                    <p className="quote">"Aim for the sky and you'll reach the ceiling. Aim for the ceiling and you'll stay on the floor."</p>
+                    <p className="bill">Bill Shankly - Liverpool F.C. Manager (1959–1974)</p>
                 
                 </div>
             </div>
